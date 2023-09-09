6707adaa4ea7f4ea2e17cff91378f4bf63c9f432
Crowdbotics <engineering@crowdbotics.com>
2023-09-09 00:57:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-union-dev-115099",
+  url: "https://soft-union-dev-115099.botics.co",
+  api: "https://soft-union-dev-115099.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
